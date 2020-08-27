0c9ef0536d907f89b079de24b6e805e10f428be2
basilije <basilije@users.noreply.github.com>
2020-08-26 20:18:27 -0700

    Add files via upload

--- gardengroup.py
+++ gardengroup.py
@@ -7,8 +7,9 @@ import RPi.GPIO as g
 import threading as h
 import Adafruit_MCP3008 as m
 import os as o
-import time as t
+import configparser as p
 import sqlite3 as q
+from time import sleep as s
 
 
 def printException(text, exce):
@@ -70,7 +71,7 @@ def appendListAsRow(file_name, list_of_elem):
 def initializeLog(log_file_name, table_header):
     try:
         appendListAsRow(log_file_name, table_header)
-        print('-' * 57, log_file_name)
+        print('-' * 57, '\n>>>', log_file_name)
         fi = open(log_file_name, "w+").close()    
     except Exception as ex:
         printException("INITIALIZE LOG EXCEPTION ", ex)
@@ -84,10 +85,10 @@ def appendLog(log_file_name, values):
     except Exception as ex:
         printException("APPEND LOG EXCEPTION ", ex)
      
-def gpioON(gpio,s):
-    if s>0:
+def gpioON(gpio,sec):
+    if sec>0:
         g.output(gpio, g.LOW)
-        t.sleep(s)
+        s(sec)
 
 def gpioOFF(gpio):
     g.output(gpio, g.HIGH)
@@ -113,6 +114,20 @@ def fileToThread(command_file_name):
     except Exception as ex:
         printException("FILE TO THREAD EXCEPTION ", ex)
 
+def compareValues(adc_values, ad_in, cmp_sign, ad_value):
+    value1 = float(adc_values[int(ad_in)])
+    value2 = float(ad_value)
+    to_return = None
+    if cmp_sign == "<":
+        to_return = (value1 < value2)
+    if cmp_sign == ">":
+        to_return = (value1 > value2)
+    if cmp_sign == "=":
+        to_return = (value1 == value2)
+    if cmp_sign == "!=":
+        to_return = (value1 != value2)
+    return(to_return)
+
  
 # Main program loop.
 sqlite_db_folder_name = "dbs/"
@@ -127,18 +142,32 @@ mcp = getMCPDevice()
 printHeader()
 initializeLog(log_file_name, table_header)
 
-while True: 
+port_to_use = None
+port_time = d.datetime.now()
+print("port_time",port_time)
+ad_in = None
+cmp_sign = None
+ad_value = None
+on_gpio_port = None
+gpio_duration = None
+gpio_wait = None
+cfg_file_name = 'cfg.cfg'
+cfg_port_section = 'gpio'
+cfg_criteria_section = 'criteria'
+cp = p.ConfigParser()
+
+while True:
+
     # Read all the ADC.
     adc_values = [0] * 8    
     try:
         adc_values = [mcp.read_adc(i) for i in range(8)]
         appendLog(log_file_name, adc_values)
     except Exception as ex:
-        printException("READ ADC EXCEPTION ", ex)
-        
-# Pause for half a second.          
-    t.sleep(loop_sleep)
-
+        printException("READ ADC EXCEPTION ", ex)        
+       
+    s(loop_sleep)
+    
 
 # Find if there is some of the gpio-named files.     
     for ch in range_of_gpio_pins:
@@ -151,6 +180,35 @@ while True:
                 o.remove(command_file_name)            
             except Exception as ex:
                 printException("NO COMMAND FILE NAME EXCEPTION ", ex)            
+
+# AI things    
+    try:
+        # Read the configuration file 
+        cp.read(cfg_file_name)
+        for cfg_variable,cfg_value in cp.items(cfg_criteria_section):
+            if cfg_variable == 'ad_in': ad_in = cfg_value   
+            if cfg_variable == 'cmp_sign': cmp_sign = cfg_value   
+            if cfg_variable == 'ad_value': ad_value = cfg_value   
+            if cfg_variable == 'on_gpio_port': on_gpio_port  = cfg_value   
+            if cfg_variable == 'gpio_duration': gpio_duration = cfg_value   
+            if cfg_variable == 'gpio_wait': gpio_wait = cfg_value   
+        for cfg_variable,cfg_value in cp.items(cfg_port_section):
+            if (on_gpio_port=='1') and cfg_variable == 'port1': port_to_use = cfg_value  
+            if (on_gpio_port=='2') and cfg_variable == 'port2': port_to_use = cfg_value  
+        res = compareValues(adc_values, ad_in, cmp_sign, ad_value)
+        if res:
+            delta_time = d.datetime.now()- port_time
+            if delta_time.total_seconds()>float(gpio_wait):
+                command_file_name = str(port_to_use) + ".txt"
+                fi = open(command_file_name, "w+")
+                fi.write(gpio_duration)
+                fi.close()
+                port_time = d.datetime.now()
+            else:
+                print("GPIO waiting...",delta_time.total_seconds(),float(gpio_wait))
+                    
+    except Exception as ex:
+        printException("AI EXCEPTION ", ex)
 
 # Write the log to the database.      
     try:   
