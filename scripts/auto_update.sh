27c0c62c0109a8b7b93f5a29d6c660d421869ef5
basilije <basilije@users.noreply.github.com>
2020-08-27 18:10:01 -0700

    Create auto_update.sh

---
+++ scripts/auto_update.sh
@@ -0,0 +1,2 @@
+curl -o /home/pi/gardengroup/gardengroup.py https://github.com/basilije/gardengroup/blob/master/home/pi/gardengroup/gardengroup.py
+sudo curl -o /var/www/html/index.php https://github.com/basilije/gardengroup/blob/master/var/www/html/index.php
